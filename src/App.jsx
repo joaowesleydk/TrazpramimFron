24d0af5793aa9d91402ba5b58510f54f9dddd978
joaowesleydk <joaodkind@gmail.com>
2025-08-01 14:43:16 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route} from"react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/home";
 import LoginPage from "./pages/loginPage";
 import RegisterPage from "./pages/RegisterPage";
 import Suporte from "./pages/suporte";
